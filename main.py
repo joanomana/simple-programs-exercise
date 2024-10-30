15acff33ca7c1fd0b4d5ac6877eb24fd2000e984
joanomana <joansebastianomasu@gmail.com>
2024-10-30 13:03:38 -0500

    Solucion ejercicio hipotenusa

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-#Escriba un programa que pida al usuario un entero de tres dígitos, y entregue el número con los dígitos en orden inverso:
-
-print("Exercise 5")
-
-number = int(input("Enter a number of 3 digits to invert: "))
-invert_number = str(number)[::-1]
-print(f"The number {number} invert is {invert_number} ")
+#Escriba un programa que reciba como entrada las longitudes de los dos catetos a y b de un triángulo rectángulo, y que entregue como salida el largo de la hipotenusa cdel triangulo, dado por el teorema de Pitágoras: c2=a2+b2
+from math import sqrt
+print("Exercise 6")
+print("Program for calculate hypotenuse")
+a = int(input("Enter a value for a: "))
+b = int(input("Enter a value for b: "))
+c=  (a**2)+(b**2)
+hypotenuse = sqrt(c)
+print(f"The hypotenuse with {a} and {b} is: {round(hypotenuse,16)}")
