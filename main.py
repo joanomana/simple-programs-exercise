93501c3700487e9d351a86f36e18e6ed57b8146d
joanomana <joansebastianomasu@gmail.com>
2024-10-30 13:06:00 -0500

    Solucion ejercicio huevo a la copa

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-print("Exercise 9")
-
+print("Exercise 10")
 import math
-note1=int(input("Enter the first grade: "))
-note2=int(input("Enter the second grade: "))
-lab=int(input("Enter the lab grade: "))
+size = str(input("Enter the size of the egg, enter g for giant or enter p for small: "))
+if size == "g":
+    m=67 #gramos
+elif size == "p":
+    m=47 #gramos
+else:
+    print("Incorrect Entry")
 
-minimun_grade = 59.5
-
-final_grade_necesary = (minimun_grade - (lab*0.3))/0.7
-print(final_grade_necesary)
-
-note3=round((final_grade_necesary*3)-(note1+note2))+1
-
-print(f"To pass the class you need {note3} in the contest")
+temp = int(input("Enter the temp of the egg: "))
+if temp <=70:
+    c= 3.7
+    p=1.038
+    k=5.4e-3
+    tw= 100
+    up= (math.pow(m,(2/3))*(c*math.pow(p,(1/3))))
+    down=(k*math.pow(math.pi,2))*((math.pow(4*(math.pi/3),(2/3))))
+    time1 =up/down
+    time2=math.log((0.76*(temp-tw)/(70-tw)))
+    time= time1*time2
+    minutes = time/60
+    print(f"The egg will take {time} seconds or {math.floor(minutes)} minutes to reach the maximum temperature")
+else:
+    print("The temp is bigger than 70, the egg cannot prepare in this temp")
