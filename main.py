20ad9bc1a67381a0eb38ba1e3707e4d76710179a
joanomana <joansebastianomasu@gmail.com>
2024-10-30 13:04:20 -0500

    Solucion ejercicio reloj

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-#Escriba un programa que reciba como entrada las longitudes de los dos catetos a y b de un triángulo rectángulo, y que entregue como salida el largo de la hipotenusa cdel triangulo, dado por el teorema de Pitágoras: c2=a2+b2
-from math import sqrt
-print("Exercise 6")
-print("Program for calculate hypotenuse")
-a = int(input("Enter a value for a: "))
-b = int(input("Enter a value for b: "))
-c=  (a**2)+(b**2)
-hypotenuse = sqrt(c)
-print(f"The hypotenuse with {a} and {b} is: {round(hypotenuse,16)}")
+#Escriba un programa que pregunte al usuario la hora actual t del reloj y un número entero de horas h, que indique qué hora marcará el reloj dentro de h horas:
+
+print("Exersice 7")
+
+hora_actual = int(input("Please enter the current time, only the hour in 12 hours format: "))
+cantidad_horas = int(input("Please enter the number of hours: "))
+
+new_hour = (hora_actual + cantidad_horas) %12
+
+print(f"In {cantidad_horas} hours it will be {new_hour} o`clock") 
