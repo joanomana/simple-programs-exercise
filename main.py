9bbe60ddae6f11f07de8ae9cad89682408b8651b
joanomana <joansebastianomasu@gmail.com>
2024-10-30 12:58:16 -0500

    Solucion ejercicio nombre

---
+++ main.py
@@ -0,0 +1,5 @@
+#Escriba un programa que pida al usuario que escriba su nombre, y lo salude llamándolo por su nombre.
+
+print("Exercise 1")
+name = str(input("Enter your name:"))
+print(f"Hello, {name}")
